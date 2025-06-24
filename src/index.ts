d0794a8c618b214cb6b6fec7e3c8bb1497bd3c30
008 <007>
2025-06-24 22:45:14 +0800

    release

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-export * from './lib/array';
-export * from './lib/cmd';
-export * from './lib/desktop';
-export * from './lib/dom';
-export * from './lib/file';
-export * from './lib/global';
-export * from './lib/object';
-export * from './lib/parallel';
-export * from './lib/rand';
-export * from './lib/string';
-export * from './lib/time';
+export * from './array';
+export * from './cmd';
+export * from './desktop';
+export * from './dom';
+export * from './file';
+export * from './global';
+export * from './object';
+export * from './parallel';
+export * from './rand';
+export * from './string';
+export * from './time';
 
 export async function copy2clipboard(text: string) {
     return navigator.clipboard.writeText(text)
