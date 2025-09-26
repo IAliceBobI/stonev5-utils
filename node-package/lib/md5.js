cf0240a948e5f86bcc92903444d2c5be20483d71
008 <007>
2025-09-26 18:59:44 +0800

    1.1.27

--- node-package/lib/md5.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMd5 = getMd5;
-const ts_md5_1 = require("ts-md5");
-function getMd5(str) {
-    const md5 = new ts_md5_1.Md5();
-    md5.appendStr(str);
-    return md5.end()?.toString() ?? "";
-}
--- node-package/lib/rand.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandFloat0tox = getRandFloat0tox;
-function getRandFloat0tox(x) {
-    return Math.random() * x;
-}
