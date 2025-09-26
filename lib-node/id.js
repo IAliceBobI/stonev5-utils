55e029932ed75829bce616d49bd5ef5c11edb375
008 <007>
2025-09-26 18:59:40 +0800

    release

--- lib-node/id.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newID = newID;
-const uuid_1 = require("uuid");
-function newID() {
-    return "ID" + (0, uuid_1.v4)().replace(/-/g, "");
-}
